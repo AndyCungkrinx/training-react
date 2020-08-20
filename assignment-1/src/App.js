a19a0f78b0be12aa7968cdf4aed500ec68f5d5bd
Raihan 2019 <raihanarsya001@gmail.com>
2020-08-20 15:08:32 +0700

    Add web socket

--- assignment-1/src/App.js
+++ assignment-1/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Websocket from 'react-websocket';
 
 // Import views
 import Home from './views/Home';
@@ -32,6 +33,7 @@ function Routing() {
 export default function App() {
   return (
     <div>
+      <Websocket url='ws://localhost:3000'/>
       <Routing />
     </div>
   );
