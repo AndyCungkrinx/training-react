9709fa34538148603f25e3199822fca2cc994586
andycungkrinx <andy@icube.us>
2020-08-24 16:02:06 +0700

    Create session2

---
+++ session-2/src/redux/reducers/userReducer.js
@@ -0,0 +1,19 @@
+const initalState = {
+    user: null,
+};
+
+const userReducer = (state = initalState, action) => {
+    const { type, payload } = action;
+    switch (type) {
+        case 'LOGIN':
+            return {
+                ...state,
+                user: payload
+            }
+        default:
+            return state;
+    }
+}
+
+export default userReducer;
+
